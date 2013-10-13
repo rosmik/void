cb466adc00218114b9b874798dfd6163048e1fb8
Micke <mickerosell@gmail.com>
2013-10-13 15:14:52 +0200

    Rebuilt Cordova test project with barcode scanner support. Can scan. Can't encode.

---
+++ web-app/platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,11 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.phonegap.plugins.barcodescanner/www/barcodescanner.js",
+        "id": "com.phonegap.plugins.barcodescanner.BarcodeScanner",
+        "clobbers": [
+            "cordova.plugins.barcodeScanner"
+        ]
+    }
+]
+});
