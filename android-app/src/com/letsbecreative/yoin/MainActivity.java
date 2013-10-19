74adcc8bd982c274195f06841852998c219329ed
Micke <mickerosell@gmail.com>
2013-10-19 20:55:42 +0200

    Merge of Database and Navigator buttons

--- android-app/src/com/letsbecreative/yoin/MainActivity.java
+++ android-app/src/com/letsbecreative/yoin/MainActivity.java
@@ -21,6 +21,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -29,6 +30,10 @@ import android.content.ComponentName;
 public class MainActivity extends FragmentActivity implements
 		ActionBar.TabListener {
 
+	// Constants used for fragment numbering
+	static final int SCANNER = 0;
+	static final int YOU = 1;
+	static final int CONTACTS = 2;
 	/**
 	 * The {@link android.support.v4.view.PagerAdapter} that will provide
 	 * fragments for each of the sections. We use a
@@ -49,7 +54,6 @@ public class MainActivity extends FragmentActivity implements
 	 */
 	Fragment fragmentTab1 = new TextInput();
 	User cardListTab = new User();
-	Fragment fragmentTab3 = new FragmentTab3();
 	QRFragment qrReaderTab = new QRFragment();
 	
 	DatabaseHandler databaseHandler;
@@ -60,7 +64,7 @@ public class MainActivity extends FragmentActivity implements
 			public void run(){
 				Toast.makeText(getApplicationContext(), "Callback called with card: " + card.toString(), Toast.LENGTH_LONG).show();
 				addCard(card);
-				mViewPager.setCurrentItem(1);
+				mViewPager.setCurrentItem(CONTACTS);
 			}
 		});
 	}
@@ -90,7 +94,9 @@ public class MainActivity extends FragmentActivity implements
 
 		// Set up the action bar.
 		final ActionBar actionBar = getActionBar();
-		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD); // NAVIGATION_MODE_STANDARD hides the tabs
+		
+		
 		
 		// Create the adapter that will return a fragment for each of the three
 		// primary sections of the app.
@@ -100,30 +106,41 @@ public class MainActivity extends FragmentActivity implements
 		// Set up the ViewPager with the sections adapter.
 		mViewPager = (ViewPager) findViewById(R.id.pager);
 		mViewPager.setAdapter(mSectionsPagerAdapter);
+		
+		// Set the default fragment to show. Showing the middle fragmet 
+		mViewPager.setCurrentItem(YOU);
+		
+		// Set up the navigation buttons in the bottom
+		// Scanner-button
+		final Button navScanButton = (Button) findViewById(R.id.nav_scanner);
+		navScanButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				// Change to Scanner fragment
+				mViewPager.setCurrentItem(SCANNER);
+			}
+		});
 
-		// When swiping between different sections, select the corresponding
-		// tab. We can also use ActionBar.Tab#select() to do this if we have
-		// a reference to the Tab.
-		mViewPager
-				.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
-					@Override
-					public void onPageSelected(int position) {
-						actionBar.setSelectedNavigationItem(position);
-					}
-				});
-
-		// For each of the sections in the app, add a tab to the action bar.
-		for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
-			// Create a tab with text corresponding to the page title defined by
-			// the adapter. Also specify this Activity object, which implements
-			// the TabListener interface, as the callback (listener) for when
-			// this tab is selected.
-			actionBar.addTab(actionBar.newTab()
-					.setText(mSectionsPagerAdapter.getPageTitle(i))
-					.setTabListener(this));
-		}
+		// You-button
+		final Button navYouButton = (Button) findViewById(R.id.nav_you);
+		navYouButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				// Change to the "You" fragment
+				mViewPager.setCurrentItem(YOU);
+			}
+		});
+		
+		// Contacts-button
+		final Button navContactsButton = (Button) findViewById(R.id.nav_contacts);
+		navContactsButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				// Change to Contacts fragment
+				mViewPager.setCurrentItem(CONTACTS);
+			}
+		});
+		
 	}
 
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		Log.d("onCreateOptionsMenu", "in optionsmenu");
@@ -181,14 +198,12 @@ public class MainActivity extends FragmentActivity implements
 			// getItem is called to instantiate the fragment for the given page.
 			// Return the different fragmenttabs
 			switch (position) {
-			case 0:
-				return fragmentTab1;
-			case 1:
-				return cardListTab;
-			case 2:
-				return fragmentTab3;
-			case 3:
+			case SCANNER:
 				return qrReaderTab;
+			case YOU:
+				return fragmentTab1;
+			case CONTACTS:
+				return cardListTab;
 			}
 			return null;
 		}
@@ -196,21 +211,19 @@ public class MainActivity extends FragmentActivity implements
 		@Override
 		public int getCount() {
 			// Show 3 total pages.
-			return 4;
+			return 3;
 		}
 
 		@Override
 		public CharSequence getPageTitle(int position) {
 			Locale l = Locale.getDefault();
 			switch (position) {
-			case 0:
+			case SCANNER:
 				return getString(R.string.title_section1).toUpperCase(l);
-			case 1:
+			case YOU:
 				return getString(R.string.title_section2).toUpperCase(l);
-			case 2:
+			case CONTACTS:
 				return getString(R.string.title_section3).toUpperCase(l);
-			case 3:
-				return getString(R.string.title_section4).toUpperCase(l);
 			}
 			return null;
 		}
